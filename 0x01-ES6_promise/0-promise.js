94940d66be16847b87dd826ac0f4c5d5c26e60a2
yaredow <yaredyilma11@gmail.com>
2022-06-16 02:47:48 -0700

    task 0 complated

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,15 +1,5 @@
-export function taskFirst() {
-    const task = 'I prefer const when I can.';
-    return task;
-  }
-  
-  export function getLast() {
-    return ' is okay';
-  }
-  
-  export function taskNext() {
-    let combination = 'But sometimes let';
-    combination += getLast();
-  
-    return combination;
+export default function getResponseFromAPI() {
+    return new Promise((resol) => {
+      resol('msg');
+    });
   }
